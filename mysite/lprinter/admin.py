c44cfe929731d649a3b0e4988eeb5cd9a9b60f24
Pavel <Pavelkasas@gmail.com>
2026-04-07 14:09:55 +0300

    padarytas model aprasimas ir amdine uzregintas

--- mysite/lprinter/admin.py
+++ mysite/lprinter/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Duties, Worker, Work, Product
 
 # Register your models here.
+
+
+admin.site.register(Duties)
+admin.site.register(Worker)
+admin.site.register(Work)
+admin.site.register(Product)
--- mysite/lprinter/models.py
+++ mysite/lprinter/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Duties (models.Model):
+    name = models.CharField(verbose_name="Pareigos")
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(models.Model):
+    first_name = models.CharField(verbose_name="Vardas")
+    last_name = models.CharField(verbose_name="Pavarde")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class  Work(models.Model):
+    task = models.CharField(verbose_name="Užduotis")
+    worker = models.ForeignKey(to="Worker", on_delete=models.SET_NULL, null=True, blank=True)
+    duties = models.ManyToManyField(to="Duties", verbose_name="Pareigos")
+
+    def __str__(self):
+        return self.task
+
+
+class Product(models.Model):
+    work = models.ForeignKey(to="Work", verbose_name="Darbuotojas", on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.DateField(verbose_name="Paslauga", null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('p', 'Priskirtas'),
+        ('i', 'Įvykdytas'),
+        ('l', 'Laisvas'),
+    )
+
+    status = models.CharField(verbose_name='Statusas_darbu', max_length=1, choices=LOAN_STATUS, blank=True, default='l')
