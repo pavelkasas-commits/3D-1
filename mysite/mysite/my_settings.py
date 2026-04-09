6eafaf631e4639f5bdb418052f03017b9ea02062
Pavel <Pavelkasas@gmail.com>
2026-04-09 14:11:14 +0300

    a

---
+++ mysite/mysite/my_settings.py
@@ -0,0 +1,5 @@
+SECRET_KEY = 'django-insecure-h*=$zq)^s)yu57534*(ck+6nu0v+ovzwfsd&xtzm$p^37vctlf'
+DEBUG = True
+ALLOWED_HOSTS = []
+EMAIL_HOST = 'pavelkasas@gmail.com
+EMAIL_HOST_PASSWORD = 'VerySecret'
